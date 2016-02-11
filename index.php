dd2c27b60a8ee30cc2578252cf78fdf593570d42
Cloud Project <lps117@naver.com>
2016-02-12 00:29:05 +0900

    index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="ko">
+	<head>
+		<meta charset="utf-8">
+		<script src="http://code.jquery.com/jquery-latest.js"></script>
+	</head>
+	<body>
+		<header id="header">
+			<img src="image/Logo.png" alt="logo" id="logo">
+			
+			<ul id="util">
+				<li><a href="login.php"><input type="button" value="login"></a></li>
+				<li><a href="join.php"><input type="button" value="join"></a></li>
+			</ul>
+			
+			<nav id="menu">
+				<ul>
+					<li><a href="#"> HOME </a></li>
+					<li><a href="#"> Shop </a></li>
+					<li><a href="#"> App </a></li>
+					<li><a href="#"> Communi </a></li>
+				</ul>
+			</nav>
+		</header>
+		
+		<div id="view">
+			
+		</div>
+		
+		
+	</body>
+</html>
